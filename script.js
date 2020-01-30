0e0f87e963adfc0fadd169468d7ccd1ad923a8e0
MFvanRossum <matissev@hotmail.com>
2020-01-30 13:54:00 -0500

    adds 'required' message

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log('Add validation!');
+
+let inputInvalid = document.querySelector('.input-invalid'); 
+// let inputField = document.querySelectorAll('.input-field');
+
+function fieldRequired() {
+    for (let element of document.querySelectorAll('.input-field')) {
+    if (inputInvalid = true);
+    let required = document.createElement('p')
+    required.innerText = '*Required'
+    required.classList.add('required')
+    element.appendChild(required) 
+    }
+}
+
+fieldRequired()
