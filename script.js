1a3e2d4644f7222b4efa8a6111a6f7810139d582
MFvanRossum <matissev@hotmail.com>
2020-01-30 19:03:17 -0500

    rewrites everything. yeehaw.

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 console.log('Add validation!');
 
-let inputInvalid = document.querySelector('.input-invalid'); 
-// let inputField = document.querySelectorAll('.input-field');
+let el = document.querySelector('#parking-form');
 
-function fieldRequired() {
-    for (let element of document.querySelectorAll('.input-field')) {
-    if (inputInvalid = true);
-    let required = document.createElement('p')
-    required.innerText = '*Required'
-    required.classList.add('required')
-    element.appendChild(required) 
-    }
-}
+el.addEventListener("submit", function(e){
+    console.log(el)
+    e.preventDefault();
+    let inputFields = document.querySelectorAll('.input')
+    console.log(inputFields)
+    for (let field of inputFields) {
+    console.log(field.value)
+        if (field.value === '') {
+            field.parentNode.classList.add('input-invalid')
+        }
+     }
+})
 
-fieldRequired()
+
+// function fieldRequired() {
+//     for (let element of document.querySelectorAll('.input-field')) {
+//     if (inputInvalid = 'true');
+//     let required = document.createElement('p')
+//     required.innerText = '*Required'
+//     required.classList.add('required')
+//     element.appendChild(required) 
+//     }
+// }
+
+// function nameValid() {
+//     let name = document.querySelector('#name')
+//     let parentDiv = name.parentNode;
+//     if (name.value !== '') {
+//     parentDiv.classList.remove('input-invalid') 
+//     parentDiv.classList.add('input-valid')
+//     }
+// }
+
+// // emptyInvalid()
+// nameValid()
+// fieldRequired()
